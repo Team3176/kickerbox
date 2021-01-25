2ab06c93a2d55cece15e6eead16e1c6d33c7dbde
Daniel McKenna <dmckenn2@gmail.com>
2021-01-25 16:06:19 -0500

    LCD Initial Hello World
    Added LCD panel
    Broke the following:
    + Serial Port
    + Buttons
    + PWM Write
    
    Resolve TODOs before merging to main

--- kickerBoxArduinoMain/kickerBoxArduinoMain.ino
+++ kickerBoxArduinoMain/kickerBoxArduinoMain.ino
@@ -1,5 +1,4 @@
-#include <SoftwareSerial.h>
-
+#include <LiquidCrystal.h>
 #include <Servo.h>
 
 
@@ -23,21 +22,27 @@ enum mode{
 };
 mode curMode = estop;
 
-
+//Declare an LCD, WE are using pins 8,9,4,5,6, and 7 (https://wiki.dfrobot.com/Arduino_LCD_KeyPad_Shield__SKU__DFR0009_#:~:text=The%20arduino%20LCD%20Keypad%20shield,%2C%20right%2C%20down%20and%20left.
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 
 
 void setup()
 {
     Serial.begin(9600);
+
+
+     lcd.begin(16, 2);              // start the library
+     lcd.setCursor(0,0);
+     lcd.print("LCD BOOT"); // print a simple message
     
     pinMode(2, INPUT);
-    pinMode(4, INPUT);
+    //pinMode(4, INPUT); //TODO Relocate
     pinMode(3,INPUT);
     digitalWrite(2,HIGH);
-    digitalWrite(4,HIGH);
+    //digitalWrite(4,HIGH); //TODO Relocate
     digitalWrite(3,HIGH);
-    motorController.attach(9,1000,2000); 
+    //motorController.attach(9,1000,2000); //TODO RELOCATE
 }
 
 
@@ -47,7 +52,7 @@ void loop()
   //inputs
   rawSlider = analogRead(sliderPort);
   clearEstop = digitalRead(2);
-  seteStop = digitalRead(4);
+  //seteStop = digitalRead(4) //TODO relocate
   //******processing*******
   //+Processing Slider Into Motor Percent
   floatSlider = rawSlider;
@@ -122,17 +127,21 @@ void loop()
   motorController.write(mcOutput);
 
 
+  lcd.setCursor(0,0);
+  lcd.print("HELLO           ");
+  lcd.setCursor(0,1);
+  lcd.print("WORLD           ");
   
-  //
-  Serial.print("Button ");
-  Serial.print(clearEstop);
-  Serial.print(" Mode ");
-  Serial.print(curMode);
-  Serial.print(" The Slider is ");
-  Serial.print(rawSlider);
-  Serial.print(" The Motor is ");
-  Serial.print(percentMotor);
-  Serial.print(" The Angle Is "); 
-  Serial.print(mcOutput);
-  Serial.println(" ");
+  // //TODO Switch Serial (Is it possible to use both simultaneously?)
+//  Serial.print("Button ");
+//  Serial.print(clearEstop);
+//  Serial.print(" Mode ");
+//  Serial.print(curMode);
+//  Serial.print(" The Slider is ");
+//  Serial.print(rawSlider);
+//  Serial.print(" The Motor is ");
+//  Serial.print(percentMotor);
+//  Serial.print(" The Angle Is "); 
+//  Serial.print(mcOutput);
+//  Serial.println(" ");
  }
